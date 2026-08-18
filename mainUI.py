85da1caf898dcf547ef8e5d538c968101218df96
Sheldon1436 <bh547516@georgetownisd.org>
2026-08-18 13:59:27 -0500

    added UI

---
+++ mainUI.py
@@ -0,0 +1,39 @@
+import random as ran
+import tkinter as tk
+from data import random_mob
+
+def get_player_count():
+    player_count = int(entry_box.get())
+
+    players = []
+
+    for i in range(player_count):
+        players.append(i)
+
+    morphs = []
+
+    for each in players:
+        variant = ran.choice(random_mob())
+        morphs.append((variant))
+
+
+    question.destroy()
+    entry_box.destroy()
+    enter.destroy()
+
+    for each in players:
+        label = tk.Label(root, text=f"Player {each+1}: {morphs[each]}")
+        label.pack(pady=5)
+
+root = tk.Tk()
+root.title("Mob Randomizer")
+root.geometry("400x300")
+
+question = tk.Label(root, text="How many players are there?")
+question.pack(pady=10)
+entry_box = tk.Entry(root, width=30)
+entry_box.pack(pady=10)
+enter = tk.Button(root, text="Enter", command=lambda: get_player_count())
+enter.pack(pady=10)
+
+root.mainloop()
