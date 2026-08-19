d0494b1f67962e370e9a34c0e4f53d40dec0f633
Sheldon1436 <bh547516@georgetownisd.org>
2026-08-19 18:09:16 -0500

    added option to not show quotation marks
    also fixed shulker + sheep being swapped in data

--- mainUI.py
+++ mainUI.py
@@ -6,14 +6,9 @@ def get_player_count():
     try:
         player_count = int(entry_box.get())
 
-        players = []
-
-        for i in range(player_count):
-            players.append(i)
-
         morphs = []
 
-        for each in players:
+        for each in range(player_count):
             variant = ran.choice(random_mob())
             morphs.append((variant))
 
@@ -21,9 +16,14 @@ def get_player_count():
         question.pack_forget()
         entry_box.pack_forget()
         enter.pack_forget()
+        check_box.pack_forget()
 
-        for each in players:
-            label = tk.Entry(root, textvariable=tk.StringVar(value=morphs[each]), state="readonly", bd=0, fg="#FFFFFF",readonlybackground=root.cget("bg"), font=("Arial", 12))
+        for each in range(player_count):
+            if show_quotes.get():
+                text =  f'"{morphs[each]}"'
+            else:
+                text = morphs[each]
+            label = tk.Entry(root, textvariable=tk.StringVar(value=text), state="readonly", bd=0, fg="#FFFFFF",readonlybackground=root.cget("bg"), font=("Arial", 12))
             label.pack(pady=5, fill="x")
 
         done = tk.Button(root, text="Done", command=lambda: run())
@@ -31,19 +31,17 @@ def get_player_count():
 
         root.update_idletasks()
         
-    except: None
+    except ValueError: None
 
 def run():
-    global question, entry_box, enter
+    global question, entry_box, enter, check_box
     for each in root.winfo_children():
-        if each not in [question, entry_box, enter]:
+        if each not in [question, entry_box, enter, check_box]:
             each.destroy()
-    question = tk.Label(root, text="How many players are there?")
     question.pack(pady=10)
-    entry_box = tk.Entry(root, width=30)
     entry_box.pack(pady=10)
-    enter = tk.Button(root, text="Enter", command=lambda: get_player_count())
     enter.pack(pady=10)
+    check_box.pack(pady=10)
     root.update_idletasks()
 
 root = tk.Tk()
@@ -51,11 +49,15 @@ root.title("Mob Randomizer")
 root.geometry("400x300")
 root.configure(bg="#343434")
 
-question = tk.Label(root, text="How many players are there?")
+show_quotes = tk.BooleanVar(value=False)
+
+question = tk.Label(root, text="How many player_count are there?")
 question.pack(pady=10)
 entry_box = tk.Entry(root, width=30)
 entry_box.pack(pady=10)
 enter = tk.Button(root, text="Enter", command=lambda: get_player_count())
 enter.pack(pady=10)
+check_box = tk.Checkbutton(root, text="Show Quotations", variable=show_quotes)
+check_box.pack(pady=10)
 
 root.mainloop()
