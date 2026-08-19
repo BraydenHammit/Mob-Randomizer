57f98ab4c01b6fb21706571b2b0e8cb204c171b1
Sheldon1436 <bh547516@georgetownisd.org>
2026-08-18 19:14:02 -0500

    updated terminal version

--- terminal_version.py
+++ terminal_version.py
@@ -1,20 +1,42 @@
 import random as ran
 from data import random_mob
-
-playerCount = int(input("How many players are there? "))
-
-players = []
-
-for i in range(playerCount):
-    players.append(i)
+end_loop = False
 
 
-morphs = []
+while not end_loop:
+    valid = False
 
-for each in players:
-    variant = ran.choice(random_mob())
-    morphs.append((variant))
+    while not valid:
+        try:
+            playerCount = (input("How many players are there? (Q to Quit)"))
+            if playerCount.upper() == "Q":
+                end_loop = True
+            playerCount = int(playerCount)
+            valid = True
+        except ValueError:
+            print("Please enter a valid number.")
+            playerCount = 1
+            valid = False
+        if playerCount <= 0:
+            print("Please enter a valid number.")
+            playerCount = 1
+            valid = False
+
+    players = []
+
+    for i in range(playerCount):
+        players.append(i)
 
 
-for each in players:
-    print(f"Player {each+1}: {morphs[each]}")
+    morphs = []
+
+    for each in players:
+        variant = ran.choice(random_mob())
+        morphs.append((variant))
+
+    print()
+
+    for each in players:
+        print(f"Player {each+1}: {morphs[each]}")
+
+    print()
